3a67cff405178964b8f6856d8842ab3c29dc2b4e
chamodi piyumika <chamodipiyumika479@gmail.com>
2024-06-26 16:15:03 +0530

    adding my frontend file

---
+++ FrontEnd/lms/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  footerComponants from './componants/flooter';
+
+function App() {
+  return (
+    <div>
+      < footerComponants />
+    </div>
+  );
+}
+
+export default App;
