2e9d52645bfdc58e3aea85784dbacc7ade67e73c
RashinShan <acmer87@gmail.com>
2024-06-27 22:19:54 +0530

    fixed bug in css not loading on loginpage.js form

--- FrontEnd/lms/src/componants/loginPage.js
+++ FrontEnd/lms/src/componants/loginPage.js
@@ -2,35 +2,33 @@ import { useState } from "react";
 import FunctionalComHead from './header/header';
 import FunctionalComFooter from './footer/footer';
 import './css/loginpage.css';
+function FunctionalComLogin() {
+  const [name, setName] = useState("");
 
-function FunctionalComLogin(){
-
-    const [name, setName] = useState("");
-
-    const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     alert(`The name you entered was: ${name}`);
   }
 
-    return(
-        <div>
-            <FunctionalComHead />
-            <div className = "loginform">
-            <form onSubmit={handleSubmit}>
-               <label>Enter your name:
-                  <input 
-                    type="text" 
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                 />
-               </label>
-               <input type="submit" />
-            </form>
-            </div>
-            <FunctionalComFooter />
-        </div>  
-    );
+  return (
+    <div>
+      <FunctionalComHead />
+      <div className="loginform" >
+        <form onSubmit={handleSubmit} >
+          <label>Enter your name:
+            <input 
+              type="text" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              style={{ display: 'block', margin: '10px 0' }}
+            />
+          </label>
+          <input type="submit"  />
+        </form>
+      </div>
+      <FunctionalComFooter />
+    </div>  
+  );
 }
+
 export default FunctionalComLogin;
-
-
